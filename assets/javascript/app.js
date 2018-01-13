3b1ba809bc09aea8c2f94bd2b9d736c4ffa7a33e
Bob Brown <robertbernardbrown@gmail.com>
2018-01-13 17:55:54 -0500

    adds comments for clarity

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    //hardcoded for now, will grab these dynamically later
     var lat = 32.07967
     var long = -81.0927
 
@@ -25,28 +26,31 @@ $(document).ready(function () {
         var array = response.businesses;
         var infoArray = [];
 
-        var venLoop = function (arg) {
-            var venArray = [];
+        //pulls vendor info from API array
+        var vendorLoop = function (arg) {
+            var vendorArray = [];
             if (Array.isArray(array)) {
                 for (var n = 0; n < arg.length; n++) {
                     var element = arg[n].title;
-                    venArray.push(element);
+                    vendorArray.push(element);
                 }
-                return venArray;
+                return vendorArray;
             }
         }
 
-        var locLoop = function (arg) {
-            var locArray = [];
+        //pulls location info from API array
+        var locationLoop = function (arg) {
+            var locationArray = [];
             if (Array.isArray(array)) {
                 for (var m = 0; m < arg.length; m++) {
                     var element = arg[m];
-                    locArray.push(element);
+                    locationArray.push(element);
                 }
-                return locArray;
+                return locationArray;
             }
         }
 
+        //reads the is_closed data in the API array
         var isOpen = function (arg) {
             if (!arg) {
                 return "Open";
@@ -59,10 +63,10 @@ $(document).ready(function () {
             var restArray = array[i];
             var restObj = {
                 restName: restArray.name,
-                restVen: venLoop(restArray.categories),
+                restVen: vendorLoop(restArray.categories),
                 restOpen: isOpen(restArray.is_closed),
                 restCost: restArray.price,
-                restLoc: locLoop(restArray.location.display_address)
+                restLoc: locationLoop(restArray.location.display_address)
             }
 
             infoArray.push(restObj);
