e70a2d43629ae8d665e343c2a726cd51439ee277
Elaina Swann <elaina@odecisions.com>
2018-01-17 18:54:09 -0500

    Fixes basic functionality for avoiding query limit. Still needs code clean up.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
+    const MAX_RESTAURANTS = 20;
     var map, location, marker, geocoder, service;
-    var restAPIArray = [];
-    var restInfoArray = [];
+    var detailAPIArray = [];
+    var restInfoArray  = [];
 
     // Immediately (self) invoked function which initializes application after document is loaded.
     (function initialize() {
@@ -63,6 +64,18 @@ $(document).ready(function () {
         $("#addr-modal").modal("show");
     }
 
+    function convertAddr(addressObj) {
+
+        var addressStr = addressObj.street + "," + 
+                         addressObj.city + "," +
+                         addressObj.state + "," +
+                         addressObj.zipcode;
+
+        // Need to validate address here.
+
+        return addressStr;
+    }
+
     function geocodeAddr(addressStr) {
         var geocoder = new google.maps.Geocoder();
 
@@ -81,38 +94,71 @@ $(document).ready(function () {
     function processAddr() {
         event.preventDefault();
         $("#addr-modal").modal("hide");
-
-        var addressObj = new Address($("#addr-street").val().trim(),
-                                     $("#addr-city").val().trim(),
-                                     $("#addr-state").val().trim(),
-                                     $("#addr-zipcode").val());
+        
+        var addressObj = {
+            "street"  : $("#addr-street").val().trim().toUpperCase(),
+            "city"    : $("#addr-city").val().trim().toUpperCase(),
+            "state"   : $("#addr-state").val().trim().toUpperCase(),
+            "zipcode" : $("#addr-zipcode").val()
+        }
 
         console.log("Input Address: ", addressObj);
+        var addressStr = "";
 
-        (addressObj.isValid()) ? geocodeAddr(addressObj.address())
-                               : console.log("processAddr: Handle this error.");
+        if ((addressStr = convertAddr(addressObj)) !== "") {
+            geocodeAddr(addressStr);
+        }
+        else {
+            console.log("processAddr: Handle this error.");
+        }
     }
 
-    function getRestaurants() {
+    async function getRestaurants() {
+        var dummy = 0;
+
         // Use current location (global variable) to determine restaurant list.
         console.log("RL Latitude: " + location.lat());
         console.log("RL Longitude: " + location.lng());
         service = new google.maps.places.PlacesService(map);
-        nearBySearch()
-        .then(function(results) {
-            var tempArray = results.slice(0, 9);
-            return Promise.all(
-                tempArray.map(findDetail)
-            );
-        })
-        .then(function(results) {
-            console.log(results);
-            var restaurants = new Restaurants(results);
-            console.log("Info: ", restaurants);
-        })
-        .catch(function(status) {
-            alert(status);
+
+        var searchAPIArray = await nearBySearch();
+        dummy = await delayProcess();
+
+        console.log("S: ", searchAPIArray);
+
+        var tempArray1 = searchAPIArray.slice(0, 9);
+        console.log("T1: ", tempArray1);
+
+        var result1 = await processSlice(tempArray1);
+        dummy = await delayProcess();
+
+        console.log("D1: ", result1);
+
+        var tempArray2 = searchAPIArray.slice(9, 18);
+        console.log("T2: ", tempArray2);
+
+        var result2 = await processSlice(tempArray2);
+        dummy = await delayProcess();
+        
+        console.log("D2: ", result2);
+
+        result1 = result1.concat(result2);
+        console.log(result1);
+
+        restInfoArray = new Restaurants(result1);
+        console.log(restInfoArray);
+    }
+
+    function delayProcess() {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve(1);
+            }, 5000);
         });
+    }
+
+    function processSlice(array) {
+        return Promise.all(array.map(findDetail));
     }
 
     function nearBySearch() {
@@ -125,6 +171,7 @@ $(document).ready(function () {
             type: 'restaurant',
             openNow: true
         };
+
         return new Promise(function(resolve, reject) {
             service.nearbySearch(request, function(results, status) {
                 if (status == google.maps.places.PlacesServiceStatus.OK) {
@@ -149,7 +196,7 @@ $(document).ready(function () {
                 }
             });
         });
-    }
+    }               
 
     // $(document).on("click", ".btn-restaurant", populateRestInfo);
     $("#btn-addr").on("click", processAddr);
