04bcb8c2617e5dbe6ab63eebe0aef4c3e7a84a50
Elaina Swann <elaina@odecisions.com>
2018-01-20 10:10:21 -0500

    Initial functionality for restaurant list time display. Still need to fix when user crosses over to next day and restaurant is still open.

--- assets/javascript/renderList.js
+++ assets/javascript/renderList.js
@@ -1,47 +1,60 @@
 function renderList(apiArr) {
-    const ITEM_COUNT = 10;
+    const MAX_COUNT = 10;
+    const TIMEFRAME = 120; // 2 hours or 120 mins.
     var container = $("#list-container"),
         listArr = apiArr.array();
+    var nowMoment = moment();
 
     (function createList() {
         container.empty();
-        if (listArr.length > 10) {
-            for (let i = 0; i < ITEM_COUNT; i++) {
-                var row = $("<div>");
-                row.addClass("row list");
 
-                var rest = $("<div>");
-                rest.addClass("restaurant text-center");
+        for (let i = 0; i < listArr.length && i < MAX_COUNT; i++) {
+            var {sendAlert, timeLeft, closeTimeStr, isOpen24Hrs} = 
+                apiArr.isClosing(i, nowMoment, TIMEFRAME); 
 
-                var button = $("<div>");
-                button.addClass("btn btn-default btn-restaurant col-xs-12 col-sm-12 col-md-12");
-                button.attr("type", "button").attr("id", "item-" + i).attr("data-toggle", "modal").attr("data-target", "#info-modal");
-                button.text(listArr[i].nameStr);
+            var row = $("<div>");
+            row.addClass("row list");
 
-                rest.append(button);
-                row.append(rest);
-                container.append(row);
-                createMarkers(listArr[i].addressStr, listArr[i].nameStr);
-                // createMarkers(i);
+            var rest = $("<div>");
+            rest.addClass("restaurant text-center");
+
+            var button = $("<div>");
+            button.addClass("btn btn-default btn-restaurant col-xs-12 col-sm-12 col-md-8");            
+            button.attr({
+                'type':        'button',
+                'id':          'item-' + i,
+                'data-toggle': 'modal',
+                'data-target': '#info-modal',
+                'data-index' : i,
+                'data-id'    : listArr[i].place_id
+            });
+            button.text(listArr[i].nameStr);
+
+            var timeInfo = $("<div>");
+            timeInfo.addClass("time-info col-xs-12 col-sm-12 col-md-4");
+
+            var closeInfo = $("<div>");
+            closeInfo.addClass("row");
+            (isOpen24Hrs) 
+                ? closeInfo.text("Open 24 Hours")
+                : closeInfo.text("Closes at " + closeTimeStr);
+
+            timeInfo.append(closeInfo);
+
+            var alertInfo = $("<div>");
+            alertInfo.addClass("row");
+            if (sendAlert) {
+                (timeLeft > 0) 
+                    ? alertInfo.text("Closes in less than " + timeLeft + " mins")
+                    : alertInfo.text("Closing soon!");
             }
-        } else {
-            for (let i = 0; i < listArr.length; i++) {
-                var row = $("<div>");
-                row.addClass("row list");
 
-                var rest = $("<div>");
-                rest.addClass("restaurant text-center");
-
-
-                var button = $("<div>");
-                button.addClass("btn btn-default btn-restaurant col-xs-12 col-sm-12 col-md-12");
-                button.attr("type", "button").attr("id", "item-" + i).attr("data-toggle", "modal").attr("data-target", "#info-modal");
-                button.text(listArr[i].nameStr);
-                rest.append(button);
-                row.append(rest);
-                container.append(row);
-                createMarkers(listArr[i].addressStr, listArr[i].nameStr);
-            }
+            timeInfo.append(alertInfo);
+            
+            rest.append(button).append(timeInfo);
+            row.append(rest);
+            container.append(row);
+            createMarkers(listArr[i].addressStr, listArr[i].nameStr);
         }
     })();
 }
